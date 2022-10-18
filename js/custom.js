5f7b414debf25bbb9a9da02398030998624a7845
Godfrey Agunga <agunga3d@gmail.com>
2022-10-18 15:22:23 +0300

    akan name working with gender

--- js/custom.js
+++ js/custom.js
@@ -4,16 +4,18 @@ var form = document.getElementById("form-akan");
 document.getElementById("submit-btn").addEventListener("click", function () {
     //   form.submit();
     var dob = document.getElementById("dob").value;
-    var akan = getAkanName(dob)
+    var gender = document.querySelector('input[name="gender-selection"]:checked').value;
+    // alert(gender);
+
+    var akan = getAkanName(dob, gender)
     alert("Akan : " + akan)
 
 });
 
-function getAkanName(dob){
+function getAkanName(dob, gender){
     const male_akan = ["Kwasi","Kwadwo","Kwabena","Kwaku"," Yaw","Kofi","Kwame"];
     const female_akan = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
     
-
     // alert(dob);
     var CC = parseInt(dob.substring(0,2));
     var YY = parseInt(dob.substring(0,4));
@@ -23,5 +25,11 @@ function getAkanName(dob){
     var d = Math.trunc(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
     //alert("d : " + d);
 
-    return male_akan[d];
+    if(gender == "M"){
+        return male_akan[d];
+    }else if(gender == "F"){
+        return female_akan[d];
+    }else
+        return null;
+
 }
